2bfe2062155fc47bec69385438c134d23c6b8c15
weniosantana <wenio.souza123@gmail.com>
2024-02-09 09:40:33 -0300

    Adicionando User

---
+++ src/main/java/com/webservices/course/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.webservices.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webservices.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> FinAll(){
+		User u = new User(1L, "Wenio", "maria@gmail.com", "999999999", "12345");
+		
+		
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+	
+}
