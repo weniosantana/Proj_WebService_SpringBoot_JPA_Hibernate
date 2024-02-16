29534fb0ab73595ac90bb0755090f3fa36a8b4b6
weniosantana <wenio.souza123@gmail.com>
2024-02-16 10:19:08 -0300

    Adicionando pedido e padronizando ISO 8601

--- src/main/java/com/webservices/course/config/TestConfig.java
+++ src/main/java/com/webservices/course/config/TestConfig.java
@@ -1,5 +1,7 @@
 package com.webservices.course.config;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.webservices.course.entities.Order;
 import com.webservices.course.entities.User;
+import com.webservices.course.repositories.OrderRepository;
 import com.webservices.course.repositories.UserRepository;
 
 @Configuration
@@ -16,16 +20,22 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
 		User u1 = new User(null, "Jose", "jose@gmail.com", "119785732", "12345");
 		User u2 = new User(null, "Joao", "joao@gmail.com", "119573474", "12345");
-
 		
+		Order o1 = new Order(null, Instant.parse("2024-02-16T09:55:42Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2024-02-16T09:58:12Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2024-02-16T10:03:37Z"), u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
 		
 	}
---
+++ src/main/java/com/webservices/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.webservices.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservices.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+	
+	
+}
