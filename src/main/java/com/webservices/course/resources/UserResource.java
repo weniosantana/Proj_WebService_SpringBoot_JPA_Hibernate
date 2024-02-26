5f4094286784feb3822d393f87797fec48a92993
weniosantana <wenio.souza123@gmail.com>
2024-02-26 11:45:12 -0300

    Adicionando metodo de inserir usuário via POST

--- src/main/java/com/webservices/course/resources/UserResource.java
+++ src/main/java/com/webservices/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.webservices.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webservices.course.entities.User;
 import com.webservices.course.services.UserService;
@@ -19,6 +23,8 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	
+	//Realizando consulta de todos os usuários por /users
 	@GetMapping
 	public ResponseEntity<List<User>> FindAll(){
 
@@ -28,11 +34,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 		
 	}
-	
+	//Realizando consulta do usuário por ID
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	//Inserindo Usuário atraves do Post
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	 
 	
 }
