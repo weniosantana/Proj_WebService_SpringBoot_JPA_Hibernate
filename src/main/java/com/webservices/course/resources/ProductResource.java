19390b732e5e5d07492bb056d8970c2037c1d171
Wenio Santana <wenio.souza123@gmail.com>
2024-02-27 18:57:58 -0300

    Adicionando e testando Post, Put, Delete de Produtos

--- src/main/java/com/webservices/course/resources/ProductResource.java
+++ src/main/java/com/webservices/course/resources/ProductResource.java
@@ -1,15 +1,22 @@
 package com.webservices.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webservices.course.entities.Product;
+import com.webservices.course.entities.User;
 import com.webservices.course.services.ProductService;
 
 @RestController
@@ -34,5 +41,26 @@ public class ProductResource {
 		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	 
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return 	ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj){
+		service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }
--- src/main/java/com/webservices/course/services/ProductService.java
+++ src/main/java/com/webservices/course/services/ProductService.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.webservices.course.entities.Product;
+import com.webservices.course.entities.User;
 import com.webservices.course.repositories.ProductRepository;
+import com.webservices.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -25,5 +29,29 @@ public class ProductService {
 		return obj.get();
 	}
 	
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
 	
+	public void delete (Long id) {
+		repository.deleteById(id);
+	}
+	
+	public Product update(Long id, Product obj) {
+		try {
+		Product entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	public Product updateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+		return entity;
+	}
 }
